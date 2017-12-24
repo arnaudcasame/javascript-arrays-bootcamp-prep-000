3143b18a558aed376ae7ba4367f4a3dee63761c7
Arnaud Casamé <djcase001@gmail.com>
2017-12-24 04:21:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(arr){
 }
 
 function removeElementFromBeginningOfArray(arr){
-  arr.slice(1);
-  return arr;
+  
+  return arr.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(arr){
